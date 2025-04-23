83683d189e17581a4b8e7aafaedc44da15f34bb4
Andrew McConachie <andrew@depht.com>
2025-04-23 13:41:42 +0000

    Fix Board

--- omega-index.sh
+++ omega-index.sh
@@ -4,7 +4,7 @@ OMINDEX=/usr/bin/omindex
 LOGFILE=/var/log/xapian-omega/omega-index.log
 
 HAM='soac/aso soac/alac soac/ccnso soac/gac soac/gnso soac/rssac soac/rzerc soac/ssac icann/ceo icann/cor icann/ext icann/ge icann/octo icann/other'
-HTML='icann/board/resolutions icann/blog/ icann/announcements'
+HTML='icann/board icann/blog/ icann/announcements'
 
 echo "$(date -I) Begin Indexing"  >> $LOGFILE
 
